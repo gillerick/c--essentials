008aefe4a3a7ae542594d3893116580c34c99ce3
Gill Erick <ogayogill95@gmail.com>
2022-12-11 16:28:05 +0300

    Add multiple input example

--- fundamentals/main.cpp
+++ fundamentals/main.cpp
@@ -11,6 +11,15 @@ int main() {
     string s;
     cin >> s;
     cout << "You typed " << s << endl; // Method 1
-    printf("You typed %s", s.c_str()); // Method 2
+    printf("You typed %s\n", s.c_str()); // Method 2
+
+    // Take in three integers and print out their sum
+    int a, b, c;
+    cout << "Enter 3 integers, separated by a whitespace" << endl;
+    cin >> a >> b >> c;
+    cout << "Sum of the three integers is " << a + b + c << endl;
+
     return 0;
+
+
 }
